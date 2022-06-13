b4357474ab0647a449aa765d9e30651ad799a849
Alberto <alberto@nada.com>
2022-06-13 18:00:20 +0200

    m1

---
+++ practica1/js/componentes.js
@@ -0,0 +1,19 @@
+
+
+
+function ElementoPrincipal() {
+    return (
+        <div>
+        <p>Me llamo Alberto</p>
+        <p>Y eres profesor </p>
+    </div>);
+};
+
+//Arrow Function
+const Ele1= ()=>{
+    return (
+        <div>
+        <p>Me llamo Alberto</p>
+        <p>Y eres profesor de REACT</p>
+    </div>);
+};
