7356dda91ec337a6788febd60c606471a00cb2a6
Alberto <alberto@nada.com>
2022-06-13 19:54:57 +0200

    m1

--- p1/src/index.js
+++ p1/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Alumno from './Componentes/Alumno';
 
-const pepito = ReactDOM.createRoot(document.getElementById('root'));
-pepito.render(
-  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
     <App />
-  
 );
 
+const barra=ReactDOM.createRoot(document.getElementById('barra'));
+barra.render(<Alumno />);
