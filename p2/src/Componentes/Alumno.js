d6d97ea0749c0ca429fd330e7b9ecf4ba31f0f69
Alberto <alberto@nada.com>
2022-06-14 15:48:01 +0200

    m1

---
+++ p2/src/Componentes/Alumno.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+function Alumno() {
+  return (
+    <div> 
+        <h2>Alumno </h2>
+    </div>
+  )
+};
+
+
+export default Alumno;
---
+++ p2/src/Componentes/Profesor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//Arrow Function
+const Profesor = () => {
+  return (
+    <div>Profesor: Pedro</div>
+  )
+};
+
+
+export default Profesor;
