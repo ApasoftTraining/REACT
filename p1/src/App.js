8565367b001f9fd37d7d510fc998c14ab06d6ea0
Alberto <alberto@nada.com>
2022-06-14 17:00:41 +0200

    m1

--- p1/src/App.js
+++ p1/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Alumno from './Componentes/Alumno';
 import Argumentos from './Componentes/Argumentos';
+import Argumentos1 from './Componentes/Argumentos1';
 import Fragmento from './Componentes/Fragmento';
 import Profesor from './Componentes/Profesor';
 import Variables from './Componentes/Variables';
@@ -19,6 +20,8 @@ function App() {
       <Variables />
       <h2>Elemento Argumentos</h2>
       <Argumentos arg1="hola" nombre="Pedro" ape1="Perez" ape2="XXXXX" />
+      <h2>Elemento Calculo salario</h2>
+      <Argumentos1 sueldo={1000} irpf={15} />
     </div>
   );
 }
