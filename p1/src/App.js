02f1795f4215e5c602c6c0bcc9798ec3c181fcad
Alberto <alberto@nada.com>
2022-06-14 15:57:35 +0200

    m1

--- p1/src/App.js
+++ p1/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Alumno from './Componentes/Alumno';
+import Fragmento from './Componentes/Fragmento';
 import Profesor from './Componentes/Profesor';
 
 function App() {
   return (
     <div>
       <h1>Ejemplo de componentes anidados</h1>
+      <Fragmento />
       <h2> Elemento Profesor</h2>
       <Profesor />
       <h2> Elemento Alumno</h2>
