f010efa331ee114fab997de9d6f9fa366f2682f9
Alberto <alberto@nada.com>
2022-06-14 15:57:26 +0200

    m1

--- p1/src/index.js
+++ p1/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Alumno from './Componentes/Alumno';
-import Profesor from './Componentes/Profesor';
+//import Alumno from './Componentes/Alumno';
+//import Profesor from './Componentes/Profesor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
