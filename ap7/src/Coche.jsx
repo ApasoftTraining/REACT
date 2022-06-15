8f7553d416dec07eaf4e3f157be149d66be07699
Alberto <alberto@nada.com>
2022-06-15 19:19:23 +0200

    m1

--- ap7/src/Coche.jsx
+++ ap7/src/Coche.jsx
@@ -4,9 +4,16 @@ import { useState } from 'react'
 function Coche() {
     //Definir un Array de Estado con Fabricantes 
   const [fabricantes, setFabricantes]=useState(['Renault','Citroen','Mercedes']);
+  const [modelos,setModelos]=useState(['coupe','Berlina','4x4']);
   const [valor,setValor]=useState("");
+  const [valorModelo,setValorModelo]=useState("");
   
-
+  function addFabricante(){
+    setFabricantes([...fabricantes,valor]);
+ }
+ function addModelo(){
+    setModelos([...modelos,valorModelo]);
+ }
 
   return (
     <div>
@@ -17,9 +24,19 @@ function Coche() {
                }
             )}            
         </ul>
-        <input type="text" name="fabri" value={valor} />
-        <button>Añadir fabricante</button>
-
+        <input type="text" name="fabri" value={valor} onChange={(e)=>setValor(e.target.value)} />
+        <button onClick={addFabricante}>Añadir fabricante</button>
+         <br>
+         </br>
+         <h2> Modelos de Coches</h2>
+         <ul>{
+               modelos.map ( function f(modelo){
+                    return <li key={modelo}>  {modelo}  </li>;
+               }
+            )}            
+        </ul>
+        <input type="text" name="fabri1" value={valorModelo} onChange={(e)=>setValorModelo(e.target.value)} />
+        <button onClick={addModelo}>Añadir modelo</button>
 
     </div>
   )
