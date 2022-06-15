bf160d7c73ef33d76eda84e50c165cbabbbde4f6
Alberto <alberto@nada.com>
2022-06-15 16:28:13 +0200

    m1

--- ap6/src/Componentes/Contador.jsx
+++ ap6/src/Componentes/Contador.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
 function Contador() {
+  let contador = 0;
 
-  let contador=0;
+  const uno = (e) => {
+    console.log(e);
+    contador = 1;
+    console.log(contador);
+    document.getElementById("cont").innerHTML=contador;
+    //document.getElementById("x").innerHTML = contador;
+  }
+
+  const dos = (e) => {
+    console.log(e);
+    contador = 2;
+    console.log(contador);
+    document.getElementById("cont").innerHTML=contador;
+   //document.getElementById("x").innerHTML = contador;
+  }
+
+
   return (
     <>
-    <h1>Contador</h1>
-    <h2> {contador} </h2>
-
+      <h1>Contador</h1>
+      <h2 id="cont"> {contador}</h2>
+      <button onClick={uno}>Poner 1</button>
+      <button onClick={dos}>Poner 2</button>
     </>
   )
 }
