1225f981172264bfb78838eef17af4d8799f0c50
Alberto <alberto@nada.com>
2022-06-16 18:25:48 +0200

    m1

--- ap8/src/Componentes/Empleado3.jsx
+++ ap8/src/Componentes/Empleado3.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Temporizador from './Temporizador';
 
 function Empleado3() {
 
   //Definir la variable de estado Empleado
-  const [empleado, setempleado] = useState({ nombre: 'Alberto',
-      apellidos: 'Rodriguez',
-      edad: 59,
-      tipo: ""
+  const [empleado, setempleado] = useState({
+    nombre: 'Alberto',
+    apellidos: 'Rodriguez',
+    edad: 59,
+    tipo: ""
 
-      });
+  });
 
   //Indicar lo siguiente según cambie la edad
   /*
@@ -21,9 +22,20 @@ function Empleado3() {
 
   */
   useEffect(() => {
-    
-  });
-  
+
+    //Cambiar el tipo
+    if (empleado.edad < 19) setempleado({ ...empleado, ["tipo"]: "niño" })
+    else if (empleado.edad < 50) setempleado({ ...empleado, ["tipo"]: "adulto" })
+    else setempleado({ ...empleado, ["tipo"]: "anciano" })
+
+    //Poner el nombre en mayúsculas
+    setempleado({...empleado,["nombre"]:empleado.nombre.toUpperCase()})
+
+  }, [empleado.edad, empleado.nombre]);
+
+
+
+
   function controlar(e) {
     setempleado({ ...empleado, [e.target.name]: e.target.value })
   }
@@ -36,18 +48,19 @@ function Empleado3() {
 
   return (
     <>
-    <form name="form1" onSubmit={grabar}>
-      <label>Nombre</label>
-      <input type="text" value={empleado.nombre} name="nombre" onChange={controlar}/>
-      <br></br>
-      <label>Apellidos</label>
-      <input type="text" value={empleado.apellidos} name="apellidos" onChange={controlar}/>
-      <br></br>
-      <label>Edad</label>
-      <input type="text" value={empleado.edad} name="edad" onChange={controlar} />
-      <br></br>
-      <input type="submit" value="Guardar" />
-    </form>
+      <form name="form1" onSubmit={grabar}>
+        <label>Nombre</label>
+        <input type="text" value={empleado.nombre} name="nombre" onChange={controlar} />
+        <br></br>
+        <label>Apellidos</label>
+        <input type="text" value={empleado.apellidos} name="apellidos" onChange={controlar} />
+        <br></br>
+        <label>Edad</label>
+        <input type="text" value={empleado.edad} name="edad" onChange={controlar} />
+        <br></br>
+        <p>Tipo: {empleado.tipo} </p>
+        <input type="submit" value="Guardar" />
+      </form>
     </>
   )
 }
