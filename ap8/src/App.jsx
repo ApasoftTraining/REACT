adb38805e0dbcb6566b8af67169e4a92e826a2f7
Alberto <alberto@nada.com>
2022-06-16 17:42:20 +0200

    m1

--- ap8/src/App.jsx
+++ ap8/src/App.jsx
@@ -2,12 +2,13 @@
 import './App.css'
 import Empleado from './Componentes/Empleado'
 import Empleado1 from './Componentes/Empleado1'
+import Empleado2 from './Componentes/Empleado2'
 import Temporizador from './Componentes/Temporizador'
 
 function App() {  
   return (
     <>
-    <Empleado1  />
+    <Empleado2  />
     <Temporizador />
 
     </>
---
+++ ap8/src/Componentes/Empleado2.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import Temporizador from './Temporizador';
+
+function Empleado2() {
+
+  //Definir la variable de estado Empleado
+  const [empleado, setempleado] = useState({
+    nombre: 'Alberto',
+    apellidos: 'Rodriguez Garcia',
+    edad: 59
+  });
+
+  //Evolucion de Empleado uando una sola funcion
+  
+  function controlar(e) {
+    setempleado({ ...empleado, [e.target.name]: e.target.value })
+  }
+
+  function grabar(evento) {
+    evento.preventDefault();
+    console.log(evento.target.name);
+    console.log(empleado);
+  }
+
+  return (
+    <>
+    <form name="form1" onSubmit={grabar}>
+      <label>Nombre</label>
+      <input type="text" value={empleado.nombre} name="nombre" onChange={controlar}/>
+      <br></br>
+      <label>Apellidos</label>
+      <input type="text" value={empleado.apellidos} name="apellidos" onChange={controlar}/>
+      <br></br>
+      <label>Edad</label>
+      <input type="text" value={empleado.edad} name="edad" onChange={controlar} />
+      <br></br>
+      <input type="submit" value="Guardar" />
+    </form>
+    </>
+  )
+}
+
+export default Empleado2
