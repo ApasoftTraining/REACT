96852db5a48e5e1b672c5292991e2aac16cec309
Alberto <alberto@nada.com>
2022-06-17 15:41:05 +0200

    m1

---
+++ ap9/src/Componentes/Fotos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Fotos() {
+  return (
+    <div> 
+        <p className="display-2">Fotos</p>
+        <button className="btn btn-primary">ACEPTAR</button>
+    </div>
+  )
+}
+
+export default Fotos
--- ap9/src/main.jsx
+++ ap9/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Fotos from './Componentes/Fotos'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Fotos />
   </React.StrictMode>
 )
