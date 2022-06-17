ee032c907ea62401b4c70a44fb19a6261152b1e3
Alberto <alberto@nada.com>
2022-06-17 16:00:58 +0200

    m1

--- ap9/src/Componentes/Fotos.jsx
+++ ap9/src/Componentes/Fotos.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import {useState} from 'react';
+import useGetFotos from '../hooks/useGetFotos';
 
 function Fotos() {
+
+  const [url, seturl] = useState('https://jsonplaceholder.typicode.com/photos');
+    
+  const resultado=useGetFotos(url);
+
   return (
     <div> 
-        <p className="display-2">Fotos</p>
+        <p className="display-2">LISTADO DE FOTOS</p>
+
+        <table className="table table-primary table-striped">
+               <tr>
+                   <th>Album</th>
+                   <th>Titulo</th>
+                   <th>imagen</th>
+               </tr>
+               {
+                resultado &&
+                resultado.map((album)=>{
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                 })
+
+
+               }
+
+        </table>
         <button className="btn btn-primary">ACEPTAR</button>
+        
     </div>
   )
 }
--- ap9/src/main.jsx
+++ ap9/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import Fotos from './Componentes/Fotos'
 import './index.css'
 
