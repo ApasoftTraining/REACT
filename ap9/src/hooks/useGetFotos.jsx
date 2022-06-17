2825e35240037df84a738a1c8bcd4e798ccf858e
Alberto <alberto@nada.com>
2022-06-17 15:30:09 +0200

    m1

---
+++ ap9/src/hooks/useGetFotos.jsx
@@ -0,0 +1,28 @@
+
+
+import { useState, useEffect } from "react";
+
+
+function useGetFotos(url) {
+
+    const [datos,setdatos]=useState(null);
+
+    useEffect(() => {
+      
+      fetch(url)
+         .then(response=> response.json())
+         .then(data =>  setdatos(data));
+         
+      return () => {
+        console.log('Carga de fotos fianlizada');
+      }
+    }, [url]);
+
+    return [datos];
+    
+  
+}
+
+export default useGetFotos;
+
+
